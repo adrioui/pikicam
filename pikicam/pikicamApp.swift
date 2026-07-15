0ff26bfc2265b9513ff191744cb4eff680abe236
adrioui <adrifdlh@gmail.com>
2026-07-15 20:36:23 +0700

    Initial Commit

---
+++ pikicam/pikicamApp.swift
@@ -0,0 +1,17 @@
+//
+//  pikicamApp.swift
+//  pikicam
+//
+//  Created by Adri Fadilah on 15/07/26.
+//
+
+import SwiftUI
+
+@main
+struct pikicamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
